d840e253a19476c33a79312c8abd43f4b0cd0284
wpaq <wallysonpabloo@gmail.com>
2022-07-20 20:38:41 -0300

    move files to its own folders

--- src/App.js
+++ src/App.js
@@ -1,25 +1,66 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { Component } from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  state = {
+    counter: 0,
+    posts: [
+      {
+        id: 1,
+        title: "Title 1",
+        body: "Body 1",
+      },
+      {
+        id: 2,
+        title: "Title 2",
+        body: "Body 2",
+      },
+      {
+        id: 3,
+        title: "Title 3",
+        body: "Body 3",
+      },
+    ],
+  };
+
+  timeOutUpdate = null;
+
+  componentDidMount() {
+    this.handleTimeOut();
+  }
+
+  componentDidUpdate() {
+    this.handleTimeOut();
+  }
+
+  componentWillUnmount() {
+    clearTimeout(this.timeOutUpdate);
+  }
+
+  handleTimeOut = () => {
+    const { posts, counter } = this.state;
+    posts[0].title = "Title Changed";
+
+    this.timeOutUpdate = setTimeout(() => {
+      this.setState({ posts, counter: counter + 1 });
+    }, 1000);
+  };
+
+  render() {
+    const { posts, counter } = this.state;
+
+    return (
+      <div className="App">
+        <h1>{counter}</h1>
+        {posts.map((post) => (
+          <div key={post.id}>
+            <h1>{post.title}</h1>
+            <p>{post.body}</p>
+          </div>
+        ))}
+      </div>
+    );
+  }
 }
 
 export default App;
