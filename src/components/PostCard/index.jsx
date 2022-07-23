fc86ec251f52932304912f8fe54a145c91e8bf73
wpaq <wallysonpabloo@gmail.com>
2022-07-23 19:11:29 -0300

    Props and organization of components

---
+++ src/components/PostCard/index.jsx
@@ -0,0 +1,9 @@
+export const PostCard = ({ title, id, body, cover }) => (
+  <div className="post">
+    <img src={cover} alt={title} />
+    <div className="post-content">
+      <h1>{title}</h1>
+      <p>{body}</p>
+    </div>
+  </div>
+);
