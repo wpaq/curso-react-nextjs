fa4688fe7555b4f92fa33df277cd5d38ba2d954f
wpaq <wallysonpabloo@gmail.com>
2022-07-23 19:29:23 -0300

    Props and organization of components part 2

--- src/App.js
+++ src/App.js
@@ -1,29 +1,20 @@
 import "./App.css";
+
 import { Component } from "react";
-import { PostCard } from "./components/PostCard";
+import { loadPosts } from "./utils/load-posts";
+import { Posts } from "./components/Posts";
 
 class App extends Component {
   state = {
     posts: [],
   };
 
-  componentDidMount() {
-    this.loadPosts();
+  async componentDidMount() {
+    await this.loadPosts();
   }
 
   loadPosts = async () => {
-    const postsResponse = fetch("https://jsonplaceholder.typicode.com/posts");
-    const photosResponse = fetch("https://jsonplaceholder.typicode.com/photos");
-
-    const [posts, photos] = await Promise.all([postsResponse, photosResponse]);
-
-    const postsJson = await posts.json();
-    const photosJson = await photos.json();
-
-    const postsAndPhotos = postsJson.map((posts, index) => {
-      return { ...posts, cover: photosJson[index].url };
-    });
-
+    const postsAndPhotos = await loadPosts();
     this.setState({ posts: postsAndPhotos });
   };
 
@@ -32,17 +23,7 @@ class App extends Component {
 
     return (
       <section className="container">
-        <div className="posts">
-          {posts.map((post) => (
-            <PostCard
-              key={post.id}
-              title={post.title}
-              body={post.body}
-              id={post.id}
-              cover={post.cover}
-            />
-          ))}
-        </div>
+        <Posts posts={posts} />
       </section>
     );
   }
